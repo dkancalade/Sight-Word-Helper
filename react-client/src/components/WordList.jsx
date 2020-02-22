03922b015b06e78c75ab3b731e28a638bc5caae3
Dustin Ancalade <umbi24@gmail.com>
2020-02-21 22:40:04 -0800

    app successfully tests spelling of the default list of words

--- react-client/src/components/WordList.jsx
+++ react-client/src/components/WordList.jsx
@@ -2,35 +2,37 @@ import React from 'react';
 import WordDisplay from './WordDisplay.jsx';
 
 const WordList = (props) => {
-  var sightWords = props.sightWords;
-    if (props.lastWord && props.sightWords.length) {
-      const HeadingElement = (
-        <div>
-          <h4> Current List: {props.currentList} </h4>
-          <p>There are { props.sightWords.length } words to practice.</p>
-          <p>Your stats are: {`${props.stats[0]}/${props.stats[1]}`}</p>
-        </div>
-      );
-    } else {
-      if (props.sightWords.length === 0) {
-        var HeadingElement = (
-        <p>
-          You've Finished!  Your final stats are: {`${props.stats[0]}/${props.stats[1]}`}. If you missed any, try again to get a perfect score
-        </p>
-        );
-      } else {
-        var HeadingElement = (
+  const createHead = (props) => {
+    var sightWords = props.sightWords;
+      if (props.lastWord && props.sightWords.length) {
+        return (
           <div>
             <h4> Current List: {props.currentList} </h4>
             <p>There are { props.sightWords.length } words to practice.</p>
+            <p>Your stats are: {`${props.stats[0]}/${props.stats[1]}`}</p>
           </div>
         );
+      } else {
+        if (props.sightWords.length === 0) {
+          return (
+          <p>
+            You've Finished!  Your final stats are: {`${props.stats[0]}/${props.stats[1]}`}. If you missed any, try again to get a perfect score
+          </p>
+          );
+        } else {
+          return (
+            <div>
+              <h4> Current List: {props.currentList} </h4>
+              <p>There are { props.sightWords.length } words to practice.</p>
+            </div>
+          );
+        }
       }
-    }
+  };
 
 return (
   <div>
-    <HeadingElement sightWords={props.sightWords} />
+    {createHead(props)}
     {props.sightWords.map(
       (sightWord, i) => (
         <WordDisplay
