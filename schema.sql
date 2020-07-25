6c433cc70cb577f67da8ea85f45d5a3654358f76
Dustin Ancalade <umbi24@gmail.com>
2020-07-24 17:21:58 -0700

    added schema to database

--- schema.sql
+++ schema.sql
@@ -4,23 +4,29 @@ CREATE DATABASE SightWords;
 
 USE SightWords;
 
-CREATE TABLE words (
+CREATE TABLE Words (
   id int NOT NULL AUTO_INCREMENT,
-  name varchar(20) NOT NULL UNIQUE,
-  url varchar(50) NOT NULL UNIQUE,
-  set_number integer NOT NULL
-  PRIMARY KEY (ID)
+  Name varchar(20) NOT NULL UNIQUE,
+  Url varchar(50) NOT NULL UNIQUE,
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE Lists (
   id int NOT NULL AUTO_INCREMENT,
   name varchar(20) NOT NULL UNIQUE,
   url varchar(50) NOT NULL UNIQUE,
-  set_number integer NOT NULL
-  PRIMARY KEY (ID)
+  PRIMARY KEY (id)
 );
 
+CREATE TABLE Lists_Words (
+  id int NOT NULL AUTO_INCREMENT,
+  list_id int NOT NULL,
+  word_id int NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (list_id) REFERENCES Lists(id),
+  FOREIGN KEY (word_id) REFERENCES Words(id)
+);
 
 /*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
+ *    mysql -u root < schema.sql
  *  to create the database and the tables.*/
