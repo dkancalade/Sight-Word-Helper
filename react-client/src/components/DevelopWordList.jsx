24081e0d4b576de73c110c24eda66df3c0fd25a2
Dustin Ancalade <umbi24@gmail.com>
2020-08-29 15:05:03 -0700

    list renders successfully from the DevelopWordList component

--- react-client/src/components/DevelopWordList.jsx
+++ react-client/src/components/DevelopWordList.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 
-const DevelopWordList = (newList, newListName, newListSize) => {
-  const newWordList = newList.map(word => <li>word</li>);
+const DevelopWordList = ({newList, newListName, newListSize}) => {
+  const newWordList = newList.map((word, i) => <li key={`newWordList-${i}`}>{word}</li>);
   return (
     <div>
-        <h3> Please wait while your Audio Files are created or Fetched </h3>
-    </div>
-    <div>
-        <ol>
-          {newWordList}
-        </ol>
+      <div>
+          <h3> Please wait while your Audio Files are created or Fetched </h3>
+      </div>
+      <div>
+          <h1>{newListName}</h1>
+          <ul>
+            {newWordList}
+          </ul>
+      </div>
+
     </div>
   );
 };
